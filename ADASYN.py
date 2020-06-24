6e291412cce7acb22c03829efd3397ebdcb38199
mikolajguszpit <guszpit.mikolaj@gmail.com>
2020-06-24 16:21:55 +0200

    eksperymenty

--- ADASYN.py
+++ ADASYN.py
@@ -2,12 +2,16 @@ import numpy as np
 from sklearn import neighbors
 
 class ADASYN:
-    def fit_resample(self, X, y, K=5,beta=1,threshold=1):
+    def fit_resample(self, X, y, K=5, beta=1, threshold=1):
         seed = 997
         np.random.seed(seed)
 
         ms = int(np.sum(y))
         ml = len(y) - ms
+
+        if ms > ml:
+            ms, ml = ml, ms
+
         clf = neighbors.KNeighborsClassifier()
         clf.fit(X, y)
         d = np.divide(ms, ml)
@@ -22,8 +26,8 @@ class ADASYN:
                 Minority_index.append(i)
         for i in range(len(y)):
             if y[i] == 1:
-                xi = X[i, :].reshape(1,-1)
-                neighbours = clf.kneighbors(xi, n_neighbors=K+1, return_distance=False)[0]
+                xi = X[i, :].reshape(1, -1)
+                neighbours = clf.kneighbors(xi, n_neighbors=K + 1, return_distance=False)[0]
                 neighbours = neighbours[1:]
                 count = 0
                 for value in neighbours:
@@ -36,8 +40,12 @@ class ADASYN:
                 Minority_per_xi.append(minority)
                 Ri.append(count / K)
         Rhat_i = []
+        Ri_sum = sum(Ri)
         for ri in Ri:
-            rhat_i = ri / sum(Ri)
+            if ri == 1:
+                Ri_sum -= 1
+        for ri in Ri:
+            rhat_i = ri / Ri_sum
             Rhat_i.append(rhat_i)
         Gi = []
         for rhat_i in Rhat_i:
@@ -47,22 +55,20 @@ class ADASYN:
         flag = 0
         for i in range(len(y)):
             if y[i] == 1:
-                xi = X[i, :].reshape(1,-1)
+                xi = X[i, :].reshape(1, -1)
                 for j in range(Gi[flag]):
                     if Minority_per_xi[flag]:
                         index = np.random.choice(Minority_per_xi[flag])
                         xzi = X[index, :].reshape(1, -1)
                         si = xi + (xzi - xi) * np.random.uniform(0, 1)
                         syn_data.append(si)
-                    else:
-                        syn_data.append(xi)
-                flag+=1
+                flag += 1
         data = []
         labels = []
         for values in syn_data:
             data.append(values[0])
         #print("{} amount of minority class samples generated".format(len(data)))
         labels2 = np.ones([len(data), 1])
-        labels = np.concatenate([y.reshape(-1, 1),labels2])
-        data = np.concatenate([X,data])
-        return data,labels
+        labels = np.concatenate([y.reshape(-1, 1), labels2])
+        data = np.concatenate([X, data])
+        return data, labels
---
+++ porownanie_precision.py
@@ -0,0 +1,139 @@
+import numpy as np
+from ADASYN import ADASYN
+from sklearn.tree import DecisionTreeClassifier
+from imblearn.over_sampling import RandomOverSampler, SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+from strlearn.metrics import precision
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.base import clone
+from tabulate import tabulate
+from scipy.stats import ranksums
+from scipy.stats import rankdata
+from scipy.stats import ttest_ind
+dataset = ['yeast4',  'vowel0', 'glass5', 'ecoli4', 'glass-0-1-6_vs_2', 'page-blocks-1-3_vs_4', 'yeast-1-2-8-9_vs_7']
+
+clf = DecisionTreeClassifier(random_state=997)
+
+preprocs = {
+    'none': None,
+    'ADASYN': ADASYN(),
+    'SMOTE' : SMOTE(random_state=997),
+    'ROS': RandomOverSampler(random_state = 997),
+    'RUS': RandomUnderSampler(random_state = 997)
+}
+
+labels = {
+    'yeast4': None,
+    'vowel0': None,
+    'glass5': None,
+    'ecoli4': None,
+    'glass-0-1-6_vs_2': None,
+    'page-blocks-1-3_vs_4': None,
+    'yeast-1-2-8-9_vs_7': None
+}
+
+n_splits = 5
+n_repeats = 2
+
+rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=997)
+
+scores = np.zeros((len(preprocs),len(dataset),n_splits * n_repeats))
+
+for data_id, dataset in enumerate(dataset):
+    dataset = np.genfromtxt("datasets/%s.csv" % (dataset), delimiter=",")
+    X = dataset[:, :-1]
+    y = dataset[:, -1].astype(int)
+
+    for fold_id, (train, test) in enumerate(rskf.split(X,y)):
+        for preproc_id, preproc in enumerate(preprocs):
+            clf = clone(clf)
+
+            if preprocs[preproc] == None:
+                X_train, y_train = X[train], y[train]
+            else:
+                X_train, y_train = preprocs[preproc].fit_resample(X[train], y[train])
+
+            clf.fit(X_train, y_train)
+            y_pred = clf.predict(X[test])
+            scores[preproc_id, data_id, fold_id] = precision(y[test], y_pred)
+
+mean_scores = np.mean(scores, axis=2).T
+
+ranks = []
+for ms in mean_scores:
+    ranks.append(rankdata(ms).tolist())
+ranks = np.array(ranks)
+
+mean_ranks = np.mean(ranks, axis=0)
+
+alfa = .05
+w_statistic = np.zeros((len(preprocs), len(preprocs)))
+p_value = np.zeros((len(preprocs), len(preprocs)))
+
+for i in range(len(preprocs)):
+    for j in range(len(preprocs)):
+        w_statistic[i, j], p_value[i, j] = ranksums(ranks.T[i], ranks.T[j])
+
+headers = list(preprocs.keys())
+names_column = np.expand_dims(np.array(list(preprocs.keys())), axis=1)
+
+w_statistic_table = np.concatenate((names_column, w_statistic), axis=1)
+w_statistic_table = tabulate(w_statistic_table, headers, floatfmt=".2f")
+
+p_value_table = np.concatenate((names_column, p_value), axis=1)
+p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
+print("\nw-statistic:\n", w_statistic_table, "\n\np-value:\n", p_value_table)
+
+advantage = np.zeros((len(preprocs), len(preprocs)))
+advantage[w_statistic > 0] = 1
+advantage_table = tabulate(np.concatenate(
+    (names_column, advantage), axis=1), headers)
+print("\nTabela przewagi:\n", advantage_table)
+
+significance = np.zeros((len(preprocs), len(preprocs)))
+significance[p_value <= alfa] = 1
+significance_table = tabulate(np.concatenate(
+    (names_column, significance), axis=1), headers)
+print("\nMacierz istotności:\n", significance_table)
+
+names_column1 = np.expand_dims(np.array(list(labels.keys())), axis=1)
+scores_M = np.concatenate((names_column1, mean_scores), axis=1)
+scores_M = tabulate(scores_M, headers, tablefmt="2f")
+
+sign_better = significance * advantage
+sign_better_table = tabulate(np.concatenate(
+    (names_column, sign_better), axis=1), headers)
+
+t_statistic = np.zeros((len(preprocs), len(preprocs)))
+p_value1 = np.zeros((len(preprocs), len(preprocs)))
+
+mean_ranks_table = tabulate((headers, mean_ranks))
+
+print("\nPrecision score")
+print("\nUśrednionie wyniki\n", scores_M)
+print("\nUśrednione rangi\n", mean_ranks_table)
+
+data_name = list(labels.keys())
+
+for label in range(len(labels)):
+    scores_label = scores[:, label, :]
+    for i in range(len(preprocs)):
+        for j in range(len(preprocs)):
+            t_statistic[i, j], p_value1[i, j] = ttest_ind(scores_label[i], scores_label[j])
+
+    advantage1 = np.zeros((len(preprocs), len(preprocs)))
+    advantage1[t_statistic > 0] = 1
+    advantage1_table = tabulate(np.concatenate(
+        (names_column, advantage1), axis=1), headers)
+
+    significance1 = np.zeros((len(preprocs), len(preprocs)))
+    significance1[p_value1 <= alfa] = 1
+    significance1_table = tabulate(np.concatenate(
+        (names_column, significance1), axis=1), headers)
+
+    sign_better1 = significance1 * advantage1
+    sign_better_table1 = tabulate(np.concatenate(
+        (names_column, sign_better1), axis=1), headers)
+
+    print("\n Statystycznie znacząco lepszy ("+data_name[label]+"):\n", sign_better_table1)
+    print("\n")
