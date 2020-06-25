de9c677828be1aa42bfc5bf9eaf0de0e75d5c6ae
mikolajguszpit <guszpit.mikolaj@gmail.com>
2020-06-25 14:48:03 +0200

    eksperymenty4

--- porownanie_recall.py
+++ porownanie_recall.py
@@ -7,12 +7,10 @@ from strlearn.metrics import recall
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.base import clone
 from tabulate import tabulate
-from scipy.stats import ranksums
-from scipy.stats import rankdata
 from scipy.stats import ttest_ind
-dataset = ['yeast4',  'vowel0', 'glass5', 'ecoli4', 'glass-0-1-6_vs_2', 'page-blocks-1-3_vs_4', 'yeast-1-2-8-9_vs_7']
 
-clf = DecisionTreeClassifier(random_state=997)
+dataset = ['yeast4',  'vowel0', 'glass5', 'ecoli4', 'glass-0-1-6_vs_2',
+           'page-blocks-1-3_vs_4', 'yeast-1-2-8-9_vs_7', 'yeast6']
 
 preprocs = {
     'none': None,
@@ -29,13 +27,15 @@ labels = {
     'ecoli4': None,
     'glass-0-1-6_vs_2': None,
     'page-blocks-1-3_vs_4': None,
-    'yeast-1-2-8-9_vs_7': None
+    'yeast-1-2-8-9_vs_7': None,
+    'yeast6': None
 }
 
 n_splits = 5
 n_repeats = 2
 
 rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=997)
+clf = DecisionTreeClassifier(random_state=997)
 
 scores = np.zeros((len(preprocs),len(dataset),n_splits * n_repeats))
 
@@ -59,75 +59,40 @@ for data_id, dataset in enumerate(dataset):
 
 mean_scores = np.mean(scores, axis=2).T
 
-ranks = []
-for ms in mean_scores:
-    ranks.append(rankdata(ms).tolist())
-ranks = np.array(ranks)
-
-alfa = .05
-w_statistic = np.zeros((len(preprocs), len(preprocs)))
-p_value = np.zeros((len(preprocs), len(preprocs)))
-
-for i in range(len(preprocs)):
-    for j in range(len(preprocs)):
-        w_statistic[i, j], p_value[i, j] = ranksums(ranks.T[i], ranks.T[j])
-
 headers = list(preprocs.keys())
-names_column = np.expand_dims(np.array(list(preprocs.keys())), axis=1)
-
-w_statistic_table = np.concatenate((names_column, w_statistic), axis=1)
-w_statistic_table = tabulate(w_statistic_table, headers, floatfmt=".2f")
-
-p_value_table = np.concatenate((names_column, p_value), axis=1)
-p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
-print("\nw-statistic:\n", w_statistic_table, "\n\np-value:\n", p_value_table)
-
-advantage = np.zeros((len(preprocs), len(preprocs)))
-advantage[w_statistic > 0] = 1
-advantage_table = tabulate(np.concatenate(
-    (names_column, advantage), axis=1), headers)
-print("\nTabela przewagi:\n", advantage_table)
-
-significance = np.zeros((len(preprocs), len(preprocs)))
-significance[p_value <= alfa] = 1
-significance_table = tabulate(np.concatenate(
-    (names_column, significance), axis=1), headers)
-print("\nMacierz istotności:\n", significance_table)
-
-names_column1 = np.expand_dims(np.array(list(labels.keys())), axis=1)
-scores_M = np.concatenate((names_column1, mean_scores), axis=1)
+names_column = np.expand_dims(np.array(list(labels.keys())), axis=1)
+scores_M = np.concatenate((names_column, mean_scores), axis=1)
 scores_M = tabulate(scores_M, headers, tablefmt="2f")
 
 t_statistic = np.zeros((len(preprocs), len(preprocs)))
-p_value_1 = np.zeros((len(preprocs), len(preprocs)))
-
-mean_ranks = np.mean(ranks, axis=0)
-mean_ranks_table = tabulate((headers, mean_ranks))
+p_value = np.zeros((len(preprocs), len(preprocs)))
+names_column_1 = np.expand_dims(np.array(list(preprocs.keys())), axis=1)
 
 print("\nRecall score")
 print("\nUśrednionie wyniki\n", scores_M)
-print("\nUśrednione rangi\n", mean_ranks_table)
 
+alfa = 0.05
 data_name = list(labels.keys())
 
 for label in range(len(labels)):
     scores_label = scores[:, label, :]
     for i in range(len(preprocs)):
         for j in range(len(preprocs)):
-            t_statistic[i, j], p_value_1[i, j] = ttest_ind(scores_label[i], scores_label[j])
+            t_statistic[i, j], p_value[i, j] = ttest_ind(scores_label[i], scores_label[j])
 
-    advantage_1 = np.zeros((len(preprocs), len(preprocs)))
-    advantage_1[t_statistic > 0] = 1
-    advantage_1_table = tabulate(np.concatenate(
-        (names_column, advantage_1), axis=1), headers)
+    advantage = np.zeros((len(preprocs), len(preprocs)))
+    advantage[t_statistic > 0] = 1
+    advantage_table = tabulate(np.concatenate(
+        (names_column_1, advantage), axis=1), headers)
 
-    significance_1 = np.zeros((len(preprocs), len(preprocs)))
-    significance_1[p_value_1 <= alfa] = 1
-    significance_1_table = tabulate(np.concatenate(
-        (names_column, significance_1), axis=1), headers)
+    significance = np.zeros((len(preprocs), len(preprocs)))
+    significance[p_value <= alfa] = 1
+    significance_table = tabulate(np.concatenate(
+        (names_column_1, significance), axis=1), headers)
 
-    sign_better = significance_1 * advantage_1
+    sign_better = significance * advantage
     sign_better_table = tabulate(np.concatenate(
-        (names_column, sign_better), axis=1), headers)
+        (names_column_1, sign_better), axis=1), headers)
 
-    print("\n Statystycznie znacząco lepszy (" + data_name[label] + "):\n", sign_better_table, "\n\n")
+    print("\n Statystycznie znacząco lepszy (" + data_name[label] + "):\n",
+          sign_better_table, "\n\n")
